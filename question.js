7ad8c14b1c2ff3b32b1ecb0833cb451fe7e1d88a
Fbin-Rthmn <rothmann.fabian@googlemail.com>
2023-12-02 16:54:02 +0100

    first functions

---
+++ question.js
@@ -0,0 +1,18 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie wird ein wasserfeste Spanplatte fachbezogen abgekürzt?",
+        "answer_1": "P2",
+        "answer_2": "P3",
+        "answer_3": "FPY",
+        "answer_4": "VP-100",
+        "right_answer": 2
+    }
+];
