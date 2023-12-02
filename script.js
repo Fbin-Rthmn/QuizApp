62d42a07894869672b7a1b2d946bf639b3c1e1ed
Fbin-Rthmn <rothmann.fabian@googlemail.com>
2023-12-02 17:52:59 +0100

    function answer

--- script.js
+++ script.js
@@ -9,22 +9,29 @@ function init() {
 function showQuestion() {
     let question = questions[currentQuestion];
 
-    document.getElementById('question').innerHTML = question.question;
-    document.getElementById('answer_1').innerHTML = question.answer_1;
-    document.getElementById('answer_2').innerHTML = question.answer_2;
-    document.getElementById('answer_3').innerHTML = question.answer_3;
-    document.getElementById('answer_4').innerHTML = question.answer_4;
+    document.getElementById('question').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
 }
 
 
 function answer(selection) {
     let question = questions[currentQuestion];
     let selectionNumber = selection.slice(-1);
+    let idOfRightAnswer = `answer_${question['right_answer']}`;
+    console.log(idOfRightAnswer);
 
     if (selectionNumber == question['right_answer']) {
         console.log('Richtig!!!');
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+        document.getElementById(selection).parentNode.classList.add('text-white');
     } else {
         console.log('Falsch!!');
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(selection).parentNode.classList.add('text-white');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('text-white');
     }
-     
 }
