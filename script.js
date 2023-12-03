7b1493d7fdd2c7d5e7fbaf3a88f9413d01fa220c
Fbin-Rthmn <rothmann.fabian@googlemail.com>
2023-12-03 11:00:36 +0100

    resetAnswerButton and End Quiz Screen

--- script.js
+++ script.js
@@ -9,11 +9,17 @@ function init() {
 function showQuestion() {
     let question = questions[currentQuestion];
 
-    document.getElementById('question').innerHTML = question['question'];
-    document.getElementById('answer_1').innerHTML = question['answer_1'];
-    document.getElementById('answer_2').innerHTML = question['answer_2'];
-    document.getElementById('answer_3').innerHTML = question['answer_3'];
-    document.getElementById('answer_4').innerHTML = question['answer_4'];
+    if (currentQuestion >= questions.length) {
+        document.getElementById('question-endScreen').style = ''
+        document.getElementById('question-body').style = 'display: none'
+    } else {
+        document.getElementById('question-current').innerHTML = currentQuestion + 1;
+        document.getElementById('question').innerHTML = question['question'];
+        document.getElementById('answer_1').innerHTML = question['answer_1'];
+        document.getElementById('answer_2').innerHTML = question['answer_2'];
+        document.getElementById('answer_3').innerHTML = question['answer_3'];
+        document.getElementById('answer_4').innerHTML = question['answer_4'];
+    }
 }
 
 
@@ -32,4 +38,23 @@ function answer(selection) {
         document.getElementById(idOfRightAnswer).parentNode.classList.add('text-white');
     }
     document.getElementById('next-button').disabled = false;
+}
+
+
+function nextQuestion() {
+    resetAnswerButton();
+    currentQuestion++;  // z.B. der Wert wird um 1 erhöht
+    showQuestion();
+}
+
+
+function resetAnswerButton() {
+    document.getElementById('next-button').disabled = true;
+
+    for (let i = 1; i < 5; i++) {
+        const index = `answer_${i}`;
+        document.getElementById(index).parentNode.classList.remove('bg-success');
+        document.getElementById(index).parentNode.classList.remove('bg-danger');
+        document.getElementById(index).parentNode.classList.remove('text-white');
+    }
 }
